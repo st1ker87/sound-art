21b2fed8a25b6c9a5c318bd2877845d212c53009
St1ker <stefanotosatto.work@gmail.com>
2021-06-11 16:39:35 +0200

    prova push in branch

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,5 +2,16 @@
 
 	<h1>HEADER</h1>
 <header>
-	<p>Sono l'header</p>
+	<div style="display: flex; justify-content: space-around;">
+		<p style="color: red; font-size: 4em;">Sono l'header</p>
+	
+		<h2>Logo dell'header</h2>
+	
+		<ul>
+			<li>Lista pagine del sito</li>
+			<li>Seconda pagina</li>
+			<li>terza pagina</li>
+		</ul>
+	</div>
+	
 </header>
