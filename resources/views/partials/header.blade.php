f5673b5e497edf7511018a028248f1a8a9db9e85
Enrico Oliva <enrich.91@gmail.com>
2021-06-12 13:44:59 +0200

    Add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,42 @@
 <header>
 
-	<h1>HEADER</h1>
+	<nav class="navbar navbar-expand-lg">
+		  <ul class="navbar-nav ml-auto">
+			<!-- Authentication Links -->
+			@guest
+				<li class="nav-item">
+					<a class="nav-link btn btn-outline-light" href="{{ route('login') }}">{{ __('Login') }}</a>
+				</li>
+				@if (Route::has('register'))
+					<li class="nav-item">
+						<a class="nav-link btn btn-outline-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+					</li>
+				@endif
+			@else
+				<li class="nav-item dropdown">
+					<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+						{{ Auth::user()->name }}
+					</a>
+
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="{{ route('logout') }}"
+							onclick="event.preventDefault();
+											document.getElementById('logout-form').submit();">
+							{{ __('Logout') }}
+						</a>
+
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+							@csrf
+						</form>
+					</div>
+				</li>
+			@endguest
+		</ul>
+		</div>
+	  </nav>
 
 	{{-- HRADER NAVBAR START originale laravel --}}
-	<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+	{{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 		<div class="container">
 			<a class="navbar-brand" href="{{ url('/') }}">
 				{{ config('app.name', 'Laravel') }}
@@ -52,7 +85,7 @@
 				</ul>
 			</div>
 		</div>
-	</nav>
+	</nav> --}}
 	{{-- HRADER NAVBAR END --}}
 
 
