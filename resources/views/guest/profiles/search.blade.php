6e608c47997f7011f9e4569f91771ea6be509784
yuri dandrea <dandreaayuri@gmail.com>
2021-06-15 15:09:45 +0200

    allineamento

--- resources/views/guest/profiles/search.blade.php
+++ resources/views/guest/profiles/search.blade.php
@@ -38,7 +38,7 @@
               <div class="my-set btn-category">
                 <div class="box">
                   <select>
-                    <option><h3>Categories</h3></option>
+                    <option disabled selected><h3>-- Categories --</h3></option>
                     <option value="band">Band</option>
                     <option value="bass">Bass</option>
                     <option value="cello">Cello</option>
@@ -51,9 +51,18 @@
               <div class="my-set btn-category">
                 <div class="box">
                   <select>
-                    <option><h3>Genrs</h3></option>
+                    <option disabled selected><h3>-- Genrs --</h3></option>
                     <option value="rock">Rock</option>
                     <option value="punk">Punk</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="jazz">Jazz</option>
                     <option value="jazz">Jazz</option>
                     <option value="hip-hop">Hip-Hop</option>
                   </select>
@@ -64,7 +73,7 @@
               <div class="my-set btn-category">
                 <div class="box">
                   <select>
-                    <option><h3>Votes</h3></option>
+                    <option disabled selected><h3>-- Votes--</h3></option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
