6a5c3a065cc957742fca29d0f61e779b010fbf78
Salvatore Trimarchi <path72@gmail.com>
2021-06-15 21:07:26 +0200

    DEV routes (no ramo sponsorships)

--- routes/web.php
+++ routes/web.php
@@ -14,70 +14,65 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-// ############################### 
-// #         DEV  ROUTES         # 
-// ############################### 
-
-Route::get('/test', 'HomeController@test')->name('test');  // ! SOLO PER TESTARE CODICE 
-
-
-
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 // %         GUEST ROUTES        % 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
 /**
- * HOME
+ * # HOME = SIMPLE SEARCH #
+ * 
+ * home page con ricerca semplice
  * http://localhost:8000/
  */
-// # home page con ricerca semplice 
 Route::get('/', 'HomeController@index')->name('home');
 
 /**
- * SEARCH
+ * # PROFILES = ADVANCED SEARCH #
+ * 
+ * Profile CRUD: parte guest (index,show) TUTTI I PROFILI
  * http://localhost:8000/search
  */
 Route::prefix('search')   	// prefisso URI raggruppamento sezione /search/...
-	->group(function () {	// rotte specifiche search
+	->group(function () {	// rotte specifiche search = profiles
 
-		// # ricerca avanzata - atterraggio diretto senza filtri 
-		Route::get('/', 'HomeController@search')->name('search'); 		// ! index() deve puntare a view('guest.profiles.search')
+		// atterraggio diretto senza filtri 
+		Route::get('/', 'ProfileController@search')->name('search'); // ! ->name('profiles.index') || return view('guest.profiles.search');
 
-		// # ricerca avanzata - atterraggio da home page con filtri 
-		Route::post('/', 'HomeController@search_from_home')->name('search_from_home'); //  {{ route('search_from_home') }}
+		// atterraggio da home page form ricerca semplice 
+		Route::post('/', 'ProfileController@search_from_home')->name('search_from_home');  // ! return view('guest.profiles.search');
+
+		// dettaglio profilo (di chiunque per chiunque)
+		Route::get('/{slug}', 'ProfileController@show')->name('profiles.show'); // ! return view('guest.profiles.show');
 
 	});
 
 /**
- * Profile CRUD - parte guest (index, show)
- * http://localhost:8000/profiles
+ * # MESSAGES #
+ * 
+ * Message CRUD: parte guest (create,store)
+ * http://localhost:8000/messages
  */
-Route::prefix('profiles')
-	->group(function() {
+Route::prefix('messages')   // prefisso URI raggruppamento sezione /messages/...
+	->group(function () {	// rotte specifiche messages
 
-		// versione 1
-		Route::get('/', 'ProfileController@index')->name('profiles.index');		// ! index() deve puntare a view('guest.profiles.search')
-		Route::get('/{slug}', 'ProfileController@show')->name('profiles.show');	// ! show()  deve puntare a view('guest.profiles.show')
+		Route::get('/', 'MessageController@create')->name('messages.create'); // ! return view('guest.messages.create');
+		Route::post('/', 'MessageController@store')->name('messages.store');  // ! return redirect()->route('NON LO SO')->with('status','Messaggio inviato correttamente');
 		
-		// versione 2
-		Route::resource('/', ProfileController::class)->names([
-				'index' 	=> 'profiles.index',
-				'show' 		=> 'profiles.show',
-				// 'create' => 'admin.posts.create',
-				// 'store' 	=> 'admin.posts.store',
-				// 'edit' 	=> 'admin.posts.edit',
-				// 'update' => 'admin.posts.update',
-				// 'destroy' => 'admin.posts.destroy',
-			]);
+	});
+
+/**
+ * # REVIEWS #
+ * 
+ * Review CRUD: parte guest (create,store)
+ * http://localhost:8000/reviews
+ */
+Route::prefix('reviews')   // prefisso URI raggruppamento sezione /reviews/...
+	->group(function () {	// rotte specifiche reviews
+
+		Route::get('/', 'ReviewController@create')->name('reviews.create'); // ! return view('guest.reviews.create');
+		Route::post('/', 'ReviewController@store')->name('reviews.store');  // ! return redirect()->route('NON LO SO')->with('status','Recensione inviata correttamente');
 		
-	});	
-
-
-
-
-
-
-
+	});
 
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
@@ -92,8 +87,10 @@ Route::prefix('admin')   	// prefisso URI raggruppamento sezione /admin/...
 	->middleware('auth')	// controllore autenticazione
 	->group(function () {	// rotte specifiche admin
 
-		// # DASHBOARD # 
 		/**
+		 * # PROFILES = DASHBOARD #
+		 * 
+		 * Profile CRUD: parte admin (index) SOLO IL PROPRIO PROFILE
 		 * questa è la home dell'utente loggato! definita in laravel qua:
 		 * 
 		 * /app/Http/Providers/RouteServiceProvider
@@ -101,40 +98,53 @@ Route::prefix('admin')   	// prefisso URI raggruppamento sezione /admin/...
 		 *
 		 * http://localhost:8000/admin/dashboard
 		 */
-		Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
+		Route::get('/dashboard', 'ProfileController@dashboard')->name('dashboard'); // ! ->name('admin.profiles.index') || return view('admin.dashboard');
+		// servirà questa SOLO se ognuno vede il dettaglio del proprio profilo DIVERSAMENTE da come lo vede chiunque
+		// Route::get('/dashboard/{slug}', 'ProfileController@show')->name('admin.profiles.show');  // ! return view('admin.profiles.show');
 
-		// # PROFILES # 
 		/**
-		 * Profile CRUD - parte admin (create, edit, delete)
+		 * # PROFILES #
+		 * 
+		 * Profile CRUD: parte admin (create,store,edit,update,destroy) SOLO IL PROPRIO PROFILE
 		 * http://localhost:8000/admin/profiles
 		 */
 		Route::resource('/profiles', ProfileController::class)->names([
-		// 	'index' 	=> 'admin.posts.index',
-		// 	'show' 		=> 'admin.posts.show',
-			'create' 	=> 'admin.posts.create',
-			'store' 	=> 'admin.posts.store',
-			'edit' 		=> 'admin.posts.edit',
-			'update' 	=> 'admin.posts.update',
-			'destroy' 	=> 'admin.posts.destroy',
+			'create' 	=> 'admin.posts.create',	// ! return view('admin.profiles.create');
+			'store' 	=> 'admin.posts.store',		// ! return redirect()->route('dashboard')->with('status','Profilo creato correttamente');
+			'edit' 		=> 'admin.posts.edit',		// ! return view('admin.profiles.edit');
+			'update' 	=> 'admin.posts.update',	// ! return redirect()->route('dashboard')->with('status','Profilo aggiornato correttamente');
+			'destroy' 	=> 'admin.posts.destroy',	// ! return redirect()->route('dashboard')->with('status','Profilo eliminato correttamente');
+		]);
+
+		/**
+		 * # MESSAGES #
+		 * 
+		 * Message CRUD: parte admin (index,show,destroy) SOLO I PROPRI MESSAGGI RICEVUTI
+		 * http://localhost:8000/admin/messages
+		 */
+		Route::resource('/messages', MessageController::class)->names([
+			'index' 	=> 'admin.messages.index',	// ! return view('admin.messages.index');
+			'show' 		=> 'admin.messages.show',	// ! return view('admin.messages.show');
+			'destroy'	=> 'admin.messages.destroy',// ! return redirect()->route('dashboard')->with('status','Profilo eliminato correttamente');
+		]);
+
+		/**
+		 * # REVIEWS #
+		 * 
+		 * Review CRUD: parte admin (index,show) SOLO LE PROPRIE RECENSIONI RICEVUTE 
+		 * http://localhost:8000/admin/reviews
+		 */
+		Route::resource('/reviews', ReviewController::class)->names([
+			'index' 	=> 'admin.reviews.index', 	// ! return view('admin.reviews.index');
+			'show' 		=> 'admin.reviews.show',	// ! return view('admin.reviews.show');
 		]);
 
 
+		// ! POI LE ROTTE PER IL RAMO SPONSORSHIP
+		//
 
 
-
-
-		/**
-		 * Category CRUD
-		 */
-		// Route::resource('/categories', CategoryController::class)->names([
-		// 	'index' 	=> 'admin.categories.index',
-		// 	'show' 		=> 'admin.categories.show',
-		// 	'create' 	=> 'admin.categories.create',
-		// 	'store' 	=> 'admin.categories.store',
-		// 	'edit' 		=> 'admin.categories.edit',
-		// 	'update' 	=> 'admin.categories.update',
-		// 	'destroy' 	=> 'admin.categories.destroy',
-		// ]);
+		
 		/**
 		 * API con token
 		 * pannello generazione token utente loggato
@@ -142,7 +152,19 @@ Route::prefix('admin')   	// prefisso URI raggruppamento sezione /admin/...
 		 */
 		// Route::get('/profile', 'HomeController@profile')->name('admin-profile');
 		// Route::post('/profile/generate-token', 'HomeController@generateToken')->name('admin.generate_token');
+
+
+
+
 	});
 
+
+
+
+// ############################### 
+// #         DEV  ROUTES         # 
+// ############################### 
+
+Route::get('/test', 'HomeController@test')->name('test');  // ! SOLO PER TESTARE CODICE 
 
 
