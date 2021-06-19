32077b7baa8bcd094a300e0593ff7cf6e71bd8c9
Salvatore Trimarchi <path72@gmail.com>
2021-06-20 00:26:04 +0200

    DEV Sponsorship/Contract migrate/seed

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -71,6 +71,51 @@
 
 
 
+/////////////////////////////////////////////
+
+use App\Sponsorship;
+
+
+$products = [
+			[
+				'name' 			=> 'silver sponsorship',
+				'description' 	=> 'Your Profile is highlighted in our Home Page for 24 hours.',
+				'hour_duration' => 24,
+				'price' 		=> 2.99
+			],
+			[
+				'name' 			=> 'gold sponsorship',
+				'description' 	=> 'Your Profile is highlighted in our Home Page for 72 hours.',
+				'hour_duration' => 72,
+				'price' 		=> 5.99
+			],
+			[
+				'name' 			=> 'platinum sponsorship',
+				'description' 	=> 'Your Profile is highlighted in our Home Page for 144 hours.',
+				'hour_duration' => 144,
+				'price' 		=> 9.99
+			],
+		];
+
+
+		foreach ($products as $product) {
+			
+			$new_sponsorship = new Sponsorship();
+			$new_sponsorship['name'] 			= $product['name'];
+			$new_sponsorship['description'] 	= $product['description'];
+			$new_sponsorship['hour_duration'] 	= $product['hour_duration'];
+			$new_sponsorship['price'] 			= $product['price'];
+			// $new_sponsorship->save(); // ! DB writing here ! 
+
+			@dump($new_sponsorship);
+		}
+
+
+
+
+
+
+
 
 
 
@@ -85,112 +130,112 @@
  * @return json: array of profiles with aux infos
  */
 
-use App\Profile;
+// use App\Profile;
 
-// DB starting source
-$profiles = Profile::all();
+// // DB starting source
+// $profiles = Profile::all();
 
-// building $iper_profiles, array of $iper_profile 
-foreach ($profiles as $profile) {
+// // building $iper_profiles, array of $iper_profile 
+// foreach ($profiles as $profile) {
 
-	// starting properties 
-	$iper_profile = $profile->toArray(); // array, NOT laravel collection!
+// 	// starting properties 
+// 	$iper_profile = $profile->toArray(); // array, NOT laravel collection!
 
-	// adding name, surname >>> strings
-	$iper_profile['name'] 	 = $profile->user->name;
-	$iper_profile['surname'] = $profile->user->surname;
+// 	// adding name, surname >>> strings
+// 	$iper_profile['name'] 	 = $profile->user->name;
+// 	$iper_profile['surname'] = $profile->user->surname;
 
-	// warning: these are lravel collections!
-	$profile_categories = $profile->user->categories;
-	$profile_genres		= $profile->user->genres;
-	$profile_offers 	= $profile->user->offers;
-	$profile_messages 	= $profile->user->messages;
-	$profile_reviews 	= $profile->user->reviews;
-	// $profile_contracts 	= $profile->user->contracts;
+// 	// warning: these are lravel collections!
+// 	$profile_categories = $profile->user->categories;
+// 	$profile_genres		= $profile->user->genres;
+// 	$profile_offers 	= $profile->user->offers;
+// 	$profile_messages 	= $profile->user->messages;
+// 	$profile_reviews 	= $profile->user->reviews;
+// 	// $profile_contracts 	= $profile->user->contracts;
 
-	// adding categories, genres, offers >>> array of strings
-	foreach ($profile_categories as $category)	$iper_profile['categories'][]	= $category->name;
-	foreach ($profile_genres as $genre)			$iper_profile['genres'][]		= $genre->name;
-	foreach ($profile_offers as $offer)			$iper_profile['offers'][] 		= $offer->name;
+// 	// adding categories, genres, offers >>> array of strings
+// 	foreach ($profile_categories as $category)	$iper_profile['categories'][]	= $category->name;
+// 	foreach ($profile_genres as $genre)			$iper_profile['genres'][]		= $genre->name;
+// 	foreach ($profile_offers as $offer)			$iper_profile['offers'][] 		= $offer->name;
 
-	// adding messages, revies >>> array of array of strings
-	// adding average_vote, rev_count >>> strings
-	// foreach ($profile_messages as $message)		$iper_profile['messages'][]		= $message->toArray(); // array, NOT laravel collection!
-	if ($profile_reviews->isNotEmpty()) {
-		$total_vote = 0;
-		foreach ($profile_reviews as $review) {
-			// $iper_profile['reviews'][] = $review->toArray(); // array, NOT laravel collection!
-			$total_vote += $review['rev_vote'];
-		}
-		$iper_profile['average_vote'] = $total_vote/count($profile_reviews); // ! voto obbligatorio per ogni review (testo facoltativo)
-	} else {
-		$iper_profile['average_vote'] = 0;
-	}
-	$iper_profile['rev_count'] = count($profile_reviews);
+// 	// adding messages, revies >>> array of array of strings
+// 	// adding average_vote, rev_count >>> strings
+// 	// foreach ($profile_messages as $message)		$iper_profile['messages'][]		= $message->toArray(); // array, NOT laravel collection!
+// 	if ($profile_reviews->isNotEmpty()) {
+// 		$total_vote = 0;
+// 		foreach ($profile_reviews as $review) {
+// 			// $iper_profile['reviews'][] = $review->toArray(); // array, NOT laravel collection!
+// 			$total_vote += $review['rev_vote'];
+// 		}
+// 		$iper_profile['average_vote'] = $total_vote/count($profile_reviews); // ! voto obbligatorio per ogni review (testo facoltativo)
+// 	} else {
+// 		$iper_profile['average_vote'] = 0;
+// 	}
+// 	$iper_profile['rev_count'] = count($profile_reviews);
 
-	// ALLEGGERIMENTO
-	$unset_properties = [
-		'work_address','work_address_gps','phone',
-		'bio_text1','bio_text2','bio_text3','video_url','audio_url','public',
-		'created_at','updated_at'
-	];
-	foreach ($unset_properties as $unset_property) {
-		unset($iper_profile[$unset_property]);
-	}
+// 	// ALLEGGERIMENTO
+// 	$unset_properties = [
+// 		'work_address','work_address_gps','phone',
+// 		'bio_text1','bio_text2','bio_text3','video_url','audio_url','public',
+// 		'created_at','updated_at'
+// 	];
+// 	foreach ($unset_properties as $unset_property) {
+// 		unset($iper_profile[$unset_property]);
+// 	}
 
-	// a new iper_profile is born!
-	$iper_profiles[] = $iper_profile;
-}
+// 	// a new iper_profile is born!
+// 	$iper_profiles[] = $iper_profile;
+// }
 
-// tutti gli iper profiles
-@dump($iper_profiles);
-
-
+// // tutti gli iper profiles
+// @dump($iper_profiles);
 
 
 
 
-// ! ingredienti utente
-$category 	= 'Drummer'; // Mixer/Engineer
-$genre		= 'Metal'; // Rock
-$offer		= 'Recording'; // 
-$vote		= 3;
-$rev_count	= 5;
-
-if ($category)	$filters['categories']		= $category;
-if ($genre)		$filters['genres']			= $genre;
-if ($offer)		$filters['offers']			= $offer;
-if ($vote)		$filters['average_vote']	= $vote;
-if ($rev_count)	$filters['rev_count']		= $rev_count;
-
-// i filtri effettivi
-@dump($filters);
-
-// filtering iteration for each user filter
-$tmp_iper_profiles = $iper_profiles;
-foreach ($filters as $key => $value) {
-	$mode = is_numeric($value) ?  'greater' : 'contains';
-	$filtered_iper_profiles = getFilteredProfiles($tmp_iper_profiles,$key,$value,$mode);
-	$tmp_iper_profiles = $filtered_iper_profiles;
-}
-
-// gli iper profiles filtrati
-@dump($filtered_iper_profiles);
 
 
+// // ! ingredienti utente
+// $category 	= 'Drummer'; // Mixer/Engineer
+// $genre		= 'Metal'; // Rock
+// $offer		= 'Recording'; // 
+// $vote		= 3;
+// $rev_count	= 5;
+
+// if ($category)	$filters['categories']		= $category;
+// if ($genre)		$filters['genres']			= $genre;
+// if ($offer)		$filters['offers']			= $offer;
+// if ($vote)		$filters['average_vote']	= $vote;
+// if ($rev_count)	$filters['rev_count']		= $rev_count;
+
+// // i filtri effettivi
+// @dump($filters);
+
+// // filtering iteration for each user filter
+// $tmp_iper_profiles = $iper_profiles;
+// foreach ($filters as $key => $value) {
+// 	$mode = is_numeric($value) ?  'greater' : 'contains';
+// 	$filtered_iper_profiles = getFilteredProfiles($tmp_iper_profiles,$key,$value,$mode);
+// 	$tmp_iper_profiles = $filtered_iper_profiles;
+// }
+
+// // gli iper profiles filtrati
+// @dump($filtered_iper_profiles);
 
 
-function getFilteredProfiles($_array,$_key,$_value,$_mode) {
-	$filtered_array = [];
-	foreach ($_array as $item) {
-		if (array_key_exists($_key,$item)) {
-			if ($_mode == 'contains') $condition = (in_array($_value,$item[$_key]));
-			if ($_mode == 'greater')  $condition = ($item[$_key] >= $_value);
-			if ($condition) $filtered_array[] = $item;
-		}
-	}
-	return $filtered_array;
-}
+
+
+// function getFilteredProfiles($_array,$_key,$_value,$_mode) {
+// 	$filtered_array = [];
+// 	foreach ($_array as $item) {
+// 		if (array_key_exists($_key,$item)) {
+// 			if ($_mode == 'contains') $condition = (in_array($_value,$item[$_key]));
+// 			if ($_mode == 'greater')  $condition = ($item[$_key] >= $_value);
+// 			if ($condition) $filtered_array[] = $item;
+// 		}
+// 	}
+// 	return $filtered_array;
+// }
 
 /////////////////////////////////////////////
 
