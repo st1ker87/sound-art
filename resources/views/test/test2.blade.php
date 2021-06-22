fc6f2035e24f1f760ae1034f18ef4190f9291923
Salvatore Trimarchi <path72@gmail.com>
2021-06-22 15:16:25 +0200

    DEV pulsanti per dashboard

--- resources/views/test/test2.blade.php
+++ resources/views/test/test2.blade.php
@@ -1,13 +1,52 @@
 <h2>TESTACODICE #2</h2>
 {{-- ///////////////////////////////////////////// --}}
 
-  
+@extends('layouts.dashboard')
+@section('title','dashboard')
+@section('content')
+
+
+
+
+{{-- ##### QUESTA PARTE DI CALCOLI È CAMBIATA >> SOSTITUIRE IL BLOCCO ##### --}}
+@php
+	date_default_timezone_set('Europe/Rome');
+	$my_user		= Auth::user();
+	$my_profile		= $my_user->profile;
+	$my_contracts	= $my_user->contracts;
+	$is_active_sponsorship = false;
+	$is_any_contract = false;
+	foreach ($my_contracts as $my_contract) {
+		$date_start = DateTime::createFromFormat('Y-m-d H:i:s', $my_contract->date_start);
+		$date_end   = DateTime::createFromFormat('Y-m-d H:i:s', $my_contract->date_end);
+		$now 		= new DateTime();
+		if ($date_start < $now && $date_end >= $now) {
+			$is_active_sponsorship = true;
+			$is_any_contract = true;
+		}
+	}
+@endphp
+
+
+
+{{-- ##### PULSANTE PER FARE SPONSORSHIP (se non ne hai nessuna attiva) ##### --}}
+@if (!$is_active_sponsorship)
+{{-- SPONSOR YOUR PROFILE --}}
+<a class="btn btn-primary" href="{{ route('admin.sponsorships.index') }}">Sponsor your Profile</a>
+@endif
+
+{{-- ##### PULSANTE PER VEDERE I CONTRATTI PASSATI (se ce ne sono) ##### --}}
+@if ($is_any_contract)
+{{-- CHECK YOUR SPONSORSHIPS --}}
+<a class="btn btn-primary" href="{{ route('my_sponsorships') }}">Check your Sponsorships</a>	
+@endif
 
 
 
 
 
 
+@endsection
 
 
 {{-- ///////////////////////////////////////////// --}}
@@ -19,161 +58,161 @@
 /////////////////////////////////////////////
 
 
-		use App\Profile;
+	// 	use App\Profile;
 
-		// DB starting source
-		$profiles = Profile::all();
+	// 	// DB starting source
+	// 	$profiles = Profile::all();
 
-		// building $iper_profiles, array of $iper_profile 
-		foreach ($profiles as $profile) {
+	// 	// building $iper_profiles, array of $iper_profile 
+	// 	foreach ($profiles as $profile) {
 
-			// starting properties 
-			$iper_profile = $profile->toArray(); // array, NOT laravel collection!
+	// 		// starting properties 
+	// 		$iper_profile = $profile->toArray(); // array, NOT laravel collection!
 
-			// adding name, surname >>> strings
-			$iper_profile['name'] 	 = $profile->user->name;
-			$iper_profile['surname'] = $profile->user->surname;
+	// 		// adding name, surname >>> strings
+	// 		$iper_profile['name'] 	 = $profile->user->name;
+	// 		$iper_profile['surname'] = $profile->user->surname;
 
-			// warning: these are lravel collections!
-			$profile_categories = $profile->user->categories;
-			$profile_genres		= $profile->user->genres;
-			$profile_offers 	= $profile->user->offers;
-			// $profile_messages 	= $profile->user->messages;
-			$profile_reviews 	= $profile->user->reviews;
-			// $profile_contracts 	= $profile->user->contracts;
-			$profile_contracts 	= $profile->user->contracts;
+	// 		// warning: these are lravel collections!
+	// 		$profile_categories = $profile->user->categories;
+	// 		$profile_genres		= $profile->user->genres;
+	// 		$profile_offers 	= $profile->user->offers;
+	// 		// $profile_messages 	= $profile->user->messages;
+	// 		$profile_reviews 	= $profile->user->reviews;
+	// 		// $profile_contracts 	= $profile->user->contracts;
+	// 		$profile_contracts 	= $profile->user->contracts;
 
-			// adding categories, genres, offers >>> array of strings
-			foreach ($profile_categories as $category)	$iper_profile['categories'][]	= $category->name;
-			foreach ($profile_genres as $genre)			$iper_profile['genres'][]		= $genre->name;
-			foreach ($profile_offers as $offer)			$iper_profile['offers'][] 		= $offer->name;
+	// 		// adding categories, genres, offers >>> array of strings
+	// 		foreach ($profile_categories as $category)	$iper_profile['categories'][]	= $category->name;
+	// 		foreach ($profile_genres as $genre)			$iper_profile['genres'][]		= $genre->name;
+	// 		foreach ($profile_offers as $offer)			$iper_profile['offers'][] 		= $offer->name;
 
-			// adding messages >>> array of array of strings [NO!]
-			// foreach ($profile_messages as $message)		$iper_profile['messages'][]		= $message->toArray(); // array, NOT laravel collection!
+	// 		// adding messages >>> array of array of strings [NO!]
+	// 		// foreach ($profile_messages as $message)		$iper_profile['messages'][]		= $message->toArray(); // array, NOT laravel collection!
 
-			// adding reviews >>> array of array of strings [NO!]
-			// adding average_vote, rev_count >>> strings
-			if ($profile_reviews->isNotEmpty()) {
-				$total_vote = 0;
-				foreach ($profile_reviews as $review) {
-					// $iper_profile['reviews'][] = $review->toArray(); // array, NOT laravel collection!
-					$total_vote += $review['rev_vote'];
-				}
-				$iper_profile['average_vote'] = $total_vote/count($profile_reviews); // ! voto obbligatorio per ogni review (testo facoltativo)
-			} else {
-				$iper_profile['average_vote'] = 0;
-			}
-			$iper_profile['rev_count'] = count($profile_reviews);
+	// 		// adding reviews >>> array of array of strings [NO!]
+	// 		// adding average_vote, rev_count >>> strings
+	// 		if ($profile_reviews->isNotEmpty()) {
+	// 			$total_vote = 0;
+	// 			foreach ($profile_reviews as $review) {
+	// 				// $iper_profile['reviews'][] = $review->toArray(); // array, NOT laravel collection!
+	// 				$total_vote += $review['rev_vote'];
+	// 			}
+	// 			$iper_profile['average_vote'] = $total_vote/count($profile_reviews); // ! voto obbligatorio per ogni review (testo facoltativo)
+	// 		} else {
+	// 			$iper_profile['average_vote'] = 0;
+	// 		}
+	// 		$iper_profile['rev_count'] = count($profile_reviews);
 
-			// sponsorship flags
-			date_default_timezone_set('Europe/Rome');
-			$is_active_sponsorship = false;
-			$active_contract_id = null;
-			foreach ($profile_contracts as $contract) {
-				$date_start = DateTime::createFromFormat('Y-m-d H:i:s', $contract->date_start);
-				$date_end   = DateTime::createFromFormat('Y-m-d H:i:s', $contract->date_end);
-				$now 		= new DateTime();
-				if ($date_start < $now && $date_end >= $now) {
-					$is_active_sponsorship = true;
-					$active_contract_id = $contract->id;
-				}
-			}
-			$iper_profile['is_active_sponsorship'] = $is_active_sponsorship;
-			$iper_profile['active_contract_id'] = $active_contract_id;
+	// 		// sponsorship flags
+	// 		date_default_timezone_set('Europe/Rome');
+	// 		$is_active_sponsorship = false;
+	// 		$active_contract_id = null;
+	// 		foreach ($profile_contracts as $contract) {
+	// 			$date_start = DateTime::createFromFormat('Y-m-d H:i:s', $contract->date_start);
+	// 			$date_end   = DateTime::createFromFormat('Y-m-d H:i:s', $contract->date_end);
+	// 			$now 		= new DateTime();
+	// 			if ($date_start < $now && $date_end >= $now) {
+	// 				$is_active_sponsorship = true;
+	// 				$active_contract_id = $contract->id;
+	// 			}
+	// 		}
+	// 		$iper_profile['is_active_sponsorship'] = $is_active_sponsorship;
+	// 		$iper_profile['active_contract_id'] = $active_contract_id;
 
-			// less transmitted properties
-			$unset_properties = [
-				'work_address','work_address_gps','phone',
-				'bio_text1','bio_text2','bio_text3','video_url','audio_url','public',
-				'created_at','updated_at'
-			];
-			foreach ($unset_properties as $unset_property) {
-				unset($iper_profile[$unset_property]);
-			}
+	// 		// less transmitted properties
+	// 		$unset_properties = [
+	// 			'work_address','work_address_gps','phone',
+	// 			'bio_text1','bio_text2','bio_text3','video_url','audio_url','public',
+	// 			'created_at','updated_at'
+	// 		];
+	// 		foreach ($unset_properties as $unset_property) {
+	// 			unset($iper_profile[$unset_property]);
+	// 		}
 
-			// a new iper_profile is born!
-			$iper_profiles[] = $iper_profile;
-		}
+	// 		// a new iper_profile is born!
+	// 		$iper_profiles[] = $iper_profile;
+	// 	}
 
-		// query parameters 1: user's filters from axios query url
-		$category 	= null;
-		$genre		= null;
-		$offer		= null;
-		$vote		= null;
-		$rev_count	= null;
+	// 	// query parameters 1: user's filters from axios query url
+	// 	$category 	= null;
+	// 	$genre		= null;
+	// 	$offer		= null;
+	// 	$vote		= null;
+	// 	$rev_count	= null;
 
-		// query parameters 2: only profiles with sponsorship in response
-		$only_sponsorship = false; // true / false
+	// 	// query parameters 2: only profiles with sponsorship in response
+	// 	$only_sponsorship = false; // true / false
 
-		// building filter set (only not null values)
-		$filters = [];
-		// ! these keys must be equal to $iper_profile's keys!
-		if ($category)	$filters['categories']		= $category;
-		if ($genre)		$filters['genres']			= $genre;
-		if ($offer)		$filters['offers']			= $offer;
-		if ($vote)		$filters['average_vote']	= $vote;
-		if ($rev_count)	$filters['rev_count']		= $rev_count;
+	// 	// building filter set (only not null values)
+	// 	$filters = [];
+	// 	// ! these keys must be equal to $iper_profile's keys!
+	// 	if ($category)	$filters['categories']		= $category;
+	// 	if ($genre)		$filters['genres']			= $genre;
+	// 	if ($offer)		$filters['offers']			= $offer;
+	// 	if ($vote)		$filters['average_vote']	= $vote;
+	// 	if ($rev_count)	$filters['rev_count']		= $rev_count;
 
-		// filtering iteration on user's filter set 
-		$filtered_iper_profiles = $iper_profiles;
-		$tmp_iper_profiles = $filtered_iper_profiles;
-		foreach ($filters as $key => $value) {
-			$mode = is_numeric($value) ?  'greater' : 'contains';
-			$filtered_iper_profiles = getFilteredProfiles($tmp_iper_profiles,$key,$value,$mode);
-			$tmp_iper_profiles = $filtered_iper_profiles;
-		}
+	// 	// filtering iteration on user's filter set 
+	// 	$filtered_iper_profiles = $iper_profiles;
+	// 	$tmp_iper_profiles = $filtered_iper_profiles;
+	// 	foreach ($filters as $key => $value) {
+	// 		$mode = is_numeric($value) ?  'greater' : 'contains';
+	// 		$filtered_iper_profiles = getFilteredProfiles($tmp_iper_profiles,$key,$value,$mode);
+	// 		$tmp_iper_profiles = $filtered_iper_profiles;
+	// 	}
 
-		// filtered iper profile array shuffle
-		shuffle($filtered_iper_profiles);
+	// 	// filtered iper profile array shuffle
+	// 	shuffle($filtered_iper_profiles);
 
-		echo 'TUTTE selezionate dall\'utente';
-		@dump($filtered_iper_profiles);
+	// 	echo 'TUTTE selezionate dall\'utente';
+	// 	@dump($filtered_iper_profiles);
 
-		// # SPLIT BY SPONSORSHIP # 
+	// 	// # SPLIT BY SPONSORSHIP # 
 
-		// ! after shuffle !
+	// 	// ! after shuffle !
 
-		$spons_f_i_profiles = [];
-		$no_spons_f_i_profiles = [];
-		foreach ($filtered_iper_profiles as $f_i_profile) {
-			if ($f_i_profile['is_active_sponsorship'])
-				$spons_f_i_profiles[] = $f_i_profile;
-			else 
-				$no_spons_f_i_profiles[] = $f_i_profile;
-		}
+	// 	$spons_f_i_profiles = [];
+	// 	$no_spons_f_i_profiles = [];
+	// 	foreach ($filtered_iper_profiles as $f_i_profile) {
+	// 		if ($f_i_profile['is_active_sponsorship'])
+	// 			$spons_f_i_profiles[] = $f_i_profile;
+	// 		else 
+	// 			$no_spons_f_i_profiles[] = $f_i_profile;
+	// 	}
 
-		echo 'gruppo sponsorship';
-		@dump($spons_f_i_profiles);
+	// 	echo 'gruppo sponsorship';
+	// 	@dump($spons_f_i_profiles);
 
-		echo 'gruppo NO sponsorship';
-		@dump($no_spons_f_i_profiles);
+	// 	echo 'gruppo NO sponsorship';
+	// 	@dump($no_spons_f_i_profiles);
 
-		echo 'la somma delle due';
-		@dump([...$spons_f_i_profiles, ...$no_spons_f_i_profiles]);	
+	// 	echo 'la somma delle due';
+	// 	@dump([...$spons_f_i_profiles, ...$no_spons_f_i_profiles]);	
 
-		// # FILTER 2: ONLY PROFILES WITH SPONSORSHIP # 
+	// 	// # FILTER 2: ONLY PROFILES WITH SPONSORSHIP # 
 
-		if ($only_sponsorship) { // ! home page
-			$filtered_iper_profiles = $spons_f_i_profiles;
-		} else { 				// ! no home page
-			$filtered_iper_profiles = [...$spons_f_i_profiles, ...$no_spons_f_i_profiles];
-		}
+	// 	if ($only_sponsorship) { // ! home page
+	// 		$filtered_iper_profiles = $spons_f_i_profiles;
+	// 	} else { 				// ! no home page
+	// 		$filtered_iper_profiles = [...$spons_f_i_profiles, ...$no_spons_f_i_profiles];
+	// 	}
 
-		// @dump($only_sponsorship);
-		// echo 'finale con $only_sponsorship = '.$only_sponsorship;
-		// @dump($filtered_iper_profiles);
+	// 	// @dump($only_sponsorship);
+	// 	// echo 'finale con $only_sponsorship = '.$only_sponsorship;
+	// 	// @dump($filtered_iper_profiles);
 
-	function getFilteredProfiles($_array,$_key,$_value,$_mode) {
-		$filtered_array = [];
-		foreach ($_array as $item) {
-			if (array_key_exists($_key,$item)) {
-				if ($_mode == 'contains') $condition = (in_array($_value,$item[$_key]));
-				if ($_mode == 'greater')  $condition = ($item[$_key] >= $_value);
-				if ($condition) $filtered_array[] = $item;
-			}
-		}
-		return $filtered_array;
-	}
+	// function getFilteredProfiles($_array,$_key,$_value,$_mode) {
+	// 	$filtered_array = [];
+	// 	foreach ($_array as $item) {
+	// 		if (array_key_exists($_key,$item)) {
+	// 			if ($_mode == 'contains') $condition = (in_array($_value,$item[$_key]));
+	// 			if ($_mode == 'greater')  $condition = ($item[$_key] >= $_value);
+	// 			if ($condition) $filtered_array[] = $item;
+	// 		}
+	// 	}
+	// 	return $filtered_array;
+	// }
 
 /////////////////////////////////////////////
 
