0a113cdaa1ec9e2ae38d4c41cdc8bf9532eb5372
St1ker <stefanotosatto.work@gmail.com>
2021-06-26 10:43:08 +0200

    preparazione merge ste

--- resources/views/admin/profiles/show.blade.php
+++ resources/views/admin/profiles/show.blade.php
@@ -1,39 +1,3 @@
-{{------------------------------------------------------------------
-	SHOW PROFILE ADMIN - SOLO SE STESSO
-
-	vedo tutti i dettagli con CV, solo vista
-
-	____
-	{ GUEST OR ADMIN } AND { ID != SE STESSO }
-		pulsante:
-			manda un messaggio > [MessageController@create]
-			>>> VORREMMO NON APRIRE UN'ALTRA VIEW <<<
-		pulsante:
-			fai una recensione > [ReviewController@create]
-			>>> VORREMMO NON APRIRE UN'ALTRA VIEW <<<
-	_____
-	{ SOLO ADMIN } AND { ID = STESSO }
-		pulsante:
-			modifica tuo profilo > [Admin/ProfileController@edit]
-	
-
-------------------------------------------------------------------}}
-{{-- <h2>MODEL: Profile, CRUD: show, AREA: admin - DETTAGLIO SINGOLO PROFILO - SOLO SE STESSO</h2>
-<h5>URL</h5>
-<p>url: http://localhost:8000/admin/profiles/{slug} (get)</p>
-<h5>SINGOLO PROFILO PASSATO</h5>
-<p>profile->id = @php echo $profile->id @endphp</p>
-<p>profile->slug = @php echo $profile->slug @endphp</p>
-<p>dump($profile) = @dump($profile)</p>
-<h5>ALTRE TABELLE DISPONIBILI</h5>
-<p>dump($users) = @dump($users)</p>
-<p>dump($profiles) = @dump($profiles)</p>
-<p>dump($categories) = @dump($categories)</p>
-<p>dump($genres) = @dump($genres)</p>
-<p>dump($offers) = @dump($offers)</p>
-<p>dump($messages) = @dump($messages)</p>
-<p>dump($reviews) = @dump($reviews)</p> --}}
-
 {{-- dati user autenticato --}}
 @php
 	$my_user 	= Auth::user();
@@ -83,3 +47,41 @@
 @endsection
   {{-- mancante video, audio e foto
     visualizzazione form messaggio, visualizzazione review --}}
+
+
+
+	{{------------------------------------------------------------------
+	SHOW PROFILE ADMIN - SOLO SE STESSO
+
+	vedo tutti i dettagli con CV, solo vista
+
+	____
+	{ GUEST OR ADMIN } AND { ID != SE STESSO }
+		pulsante:
+			manda un messaggio > [MessageController@create]
+			>>> VORREMMO NON APRIRE UN'ALTRA VIEW <<<
+		pulsante:
+			fai una recensione > [ReviewController@create]
+			>>> VORREMMO NON APRIRE UN'ALTRA VIEW <<<
+	_____
+	{ SOLO ADMIN } AND { ID = STESSO }
+		pulsante:
+			modifica tuo profilo > [Admin/ProfileController@edit]
+	
+
+------------------------------------------------------------------}}
+{{-- <h2>MODEL: Profile, CRUD: show, AREA: admin - DETTAGLIO SINGOLO PROFILO - SOLO SE STESSO</h2>
+<h5>URL</h5>
+<p>url: http://localhost:8000/admin/profiles/{slug} (get)</p>
+<h5>SINGOLO PROFILO PASSATO</h5>
+<p>profile->id = @php echo $profile->id @endphp</p>
+<p>profile->slug = @php echo $profile->slug @endphp</p>
+<p>dump($profile) = @dump($profile)</p>
+<h5>ALTRE TABELLE DISPONIBILI</h5>
+<p>dump($users) = @dump($users)</p>
+<p>dump($profiles) = @dump($profiles)</p>
+<p>dump($categories) = @dump($categories)</p>
+<p>dump($genres) = @dump($genres)</p>
+<p>dump($offers) = @dump($offers)</p>
+<p>dump($messages) = @dump($messages)</p>
+<p>dump($reviews) = @dump($reviews)</p> --}}
