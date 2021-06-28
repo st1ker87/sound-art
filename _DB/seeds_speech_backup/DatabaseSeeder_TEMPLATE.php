9c85d2c4f133187fa1984c0e31d22ab0a2a3a2cf
Salvatore Trimarchi <path72@gmail.com>
2021-06-28 09:34:23 +0200

    DEV Speech DB seed WIP

--- _DB/seeds_speech_backup/DatabaseSeeder_TEMPLATE.php
+++ _DB/seeds_speech_backup/DatabaseSeeder_TEMPLATE.php
@@ -11,36 +11,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-		// Kate Glock
 		$this->call(SinglePersonSeeder1::class);
-		// 
 		$this->call(SinglePersonSeeder2::class);
-		//
 		$this->call(SinglePersonSeeder3::class);
-		//
 		$this->call(SinglePersonSeeder4::class);
-		//
 		$this->call(SinglePersonSeeder5::class);
-		//
-		$this->call(SinglePersonSeeder6::class);
-		//
-		$this->call(SinglePersonSeeder7::class);
-		//
-		$this->call(SinglePersonSeeder8::class);
-		//
-		$this->call(SinglePersonSeeder9::class);
-		//
-		$this->call(SinglePersonSeeder10::class);
-		//
-		$this->call(SinglePersonSeeder11::class);
-		//
-		$this->call(SinglePersonSeeder12::class);
-		//
-		$this->call(SinglePersonSeeder13::class);
-		//
-		$this->call(SinglePersonSeeder14::class);
-		//
-		$this->call(SinglePersonSeeder15::class);
-
+		// $this->call(SinglePersonSeeder6::class);
+		// $this->call(SinglePersonSeeder7::class);
+		// $this->call(SinglePersonSeeder8::class);
+		// $this->call(SinglePersonSeeder9::class);
+		// $this->call(SinglePersonSeeder10::class);
+		// $this->call(SinglePersonSeeder11::class);
+		// $this->call(SinglePersonSeeder12::class);
+		// $this->call(SinglePersonSeeder13::class);
+		// $this->call(SinglePersonSeeder14::class);
+		// $this->call(SinglePersonSeeder15::class);
+		// $this->call(SinglePersonSeeder16::class);
+		// $this->call(SinglePersonSeeder17::class);
+		// $this->call(SinglePersonSeeder18::class);
+		// $this->call(SinglePersonSeeder19::class);
+		// $this->call(SinglePersonSeeder20::class);
+		// $this->call(SinglePersonSeeder21::class);
+		// $this->call(SinglePersonSeeder22::class);
+		// $this->call(SinglePersonSeeder23::class);
+		// $this->call(SinglePersonSeeder24::class);
+		// $this->call(SinglePersonSeeder25::class);
+		// $this->call(SinglePersonSeeder26::class);
+		// $this->call(SinglePersonSeeder27::class);
+		// $this->call(SinglePersonSeeder28::class);
+		// $this->call(SinglePersonSeeder29::class);
+		// $this->call(SinglePersonSeeder30::class);
+		// $this->call(SinglePersonSeeder31::class);
+		// $this->call(SinglePersonSeeder32::class);
+		// $this->call(SinglePersonSeeder33::class);
+		// $this->call(SinglePersonSeeder34::class);
+		// $this->call(SinglePersonSeeder35::class);
+		// $this->call(SinglePersonSeeder36::class);
+		// $this->call(SinglePersonSeeder37::class);
+		// $this->call(SinglePersonSeeder38::class);
+		// $this->call(SinglePersonSeeder39::class);
+		// $this->call(SinglePersonSeeder40::class);
+		// $this->call(SinglePersonSeeder41::class);
+		// $this->call(SinglePersonSeeder42::class);
+		// $this->call(SinglePersonSeeder43::class);
+		// $this->call(SinglePersonSeeder44::class);
+		// $this->call(SinglePersonSeeder45::class);
+		// $this->call(SinglePersonSeeder46::class);
+		// $this->call(SinglePersonSeeder47::class);
+		// $this->call(SinglePersonSeeder48::class);
+		// $this->call(SinglePersonSeeder49::class);
+		// $this->call(SinglePersonSeeder50::class);
     }
 }
