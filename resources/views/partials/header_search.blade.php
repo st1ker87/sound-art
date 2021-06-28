0b83362397fec94eeac4ab8cf6e7c62791bd32f7
Enrico Oliva <enrich.91@gmail.com>
2021-06-28 17:18:32 +0200

    risolto tendina utente loggato

--- resources/views/partials/header_search.blade.php
+++ resources/views/partials/header_search.blade.php
@@ -26,6 +26,26 @@
 				<li class="nav-item">
 					<a class="nav-link" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
 				</li>
+				<li class="nav-link auth-pannel-dropdown">
+					<button v-on:click.stop="showAuth" class="auth-btn">{{ Auth::user()->name }}</button>
+					<div v-if="showAuthPannel" class="auth-pannel-cnt-blue">
+						<ul class="auth-pannel">
+							<li>
+								<a href="{{ route('logout') }}"
+										onclick="event.preventDefault();
+											document.getElementById('logout-form').submit();">
+									{{ __('Logout') }}
+								</a>
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+									@csrf
+								</form>
+							</li>
+						</ul>
+					</div>
+				</li>
+				{{-- <li class="nav-item">
+					<a class="nav-link" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
+				</li>
 				<li class="nav-item dropdown">
 					<a id="navbarDropdown btn btn-outline-light" class="nav-link dropdown-toggle" :class="{change_color_btn: scrollPosition > scrollChange}" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 						{{ Auth::user()->name }}
@@ -42,7 +62,7 @@
 							@csrf
 						</form>
 					</div>
-				</li>
+				</li> --}}
 			@endguest
 			</ul>
 		  </nav>
