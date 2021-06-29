c69fa6e0aafdb37131541981e4077191081e3dbc
yuri dandrea <dandreaayuri@gmail.com>
2021-06-29 20:32:30 +0200

    loader homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,6 +103,19 @@
         </div>
     </section>
 
+    <section class="load_sect" :class="{load_visible : load_visible == true}">
+      <div class="middle">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="bar bar4"></div>
+        <div class="bar bar5"></div>
+        <div class="bar bar6"></div>
+        <div class="bar bar7"></div>
+        <div class="bar bar8"></div>
+      </div>
+    </section>
+
     @include('partials.cards_home')
 
     <!-- Discover -->
